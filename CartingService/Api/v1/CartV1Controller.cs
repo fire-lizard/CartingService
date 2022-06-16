85e253ffaa789192447911fa3846c40f228ab116
Anatoliy Sova <sovaar@hotmail.com>
2022-06-16 09:07:13 +0300

    Controllers added

---
+++ CartingService/Api/v1/CartV1Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CartingService.BLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartingService.Api.v1
+{
+    [ApiController]  
+    [ApiVersion("1.0")]  
+    [Route("api/{v:apiVersion}/cart")]  
+    public class CartV1Controller
+    {
+        [HttpGet(Name = "GetCartInfo")]
+        public Cart Get()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost(Name = "AddItemToCart")]
+        public Cart Post()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpDelete(Name = "DeleteItemFromCart")]
+        public Cart Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CartingService/Api/v2/CartV2Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using CartingService.BLL;
+using CartingService.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartingService.Api.v2
+{
+    [ApiController]  
+    [ApiVersion("1.0")]  
+    [Route("api/{v:apiVersion}/cart")]  
+    public class CartV2Controller
+    {
+        [HttpGet(Name = "GetCartInfo")]
+        public IEnumerable<CartItem> Get()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost(Name = "AddItemToCart")]
+        public Cart Post()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpDelete(Name = "DeleteItemFromCart")]
+        public Cart Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
