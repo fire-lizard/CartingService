f0089d0b8afc2b0a291ddf5dcb4193540bf2971d
Anatoliy Sova <sovaar@hotmail.com>
2022-06-16 21:40:32 +0300

    small refactoring

--- CartingService/Api/v2/CartV2Controller.cs
+++ CartingService/Api/v2/CartV2Controller.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CartingService.Api.v2
 {
     [ApiController]  
-    [ApiVersion("1.0")]  
+    [ApiVersion("2.0")]  
     [Route("api/{v:apiVersion}/cart")]  
     public class CartV2Controller
     {
